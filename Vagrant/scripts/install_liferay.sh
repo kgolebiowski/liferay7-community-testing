fa19423f8d000e9fba96f73b55de137fb67c34bd
Krzysztof Gołębiowski <krzysztof.golebiowski@xentivo.pl>
2015-08-20 22:28:46 +0200

    Download Java and Liferay archives if not found under installs/ directory

--- Vagrant/scripts/install_liferay.sh
+++ Vagrant/scripts/install_liferay.sh
@@ -7,40 +7,42 @@
 
 echo "Provisioning from: \"install_liferay.sh\""
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
   echo "Wrong number of arguments."
-  echo "Usage: "$0" <liferay_user> <liferay_zip_archive>"
+  echo "Usage: "$0" <liferay_user> <liferay_zip_archive> <lr_download_path>"
 else
-
 	LR_USER=$1
 	LR_FILE=$2
+  LR_DOWNLOAD_PATH=$3
 
-	if [ -f /vagrant/resources/installs/$LR_FILE ]; then
-		echo "Liferay archive exists, unzipping..."
-		unzip -q /vagrant/resources/installs/$LR_FILE -d /home/$LR_USER/
+  INSTALLS_FOLDER=/vagrant/resources/installs
+  CONFIGS_FOLDER=/vagrant/resources/configs
 
-    LR_PATH=`find /home/$LR_USER/ -type d -iname 'liferay-portal*'`
-    echo "Liferay installed in: \"$LR_PATH\""
+	if [ -f $INSTALLS_FOLDER/$LR_FILE ]; then
+    echo "Liferay archive has already been downloaded."
+  else
+    echo "Liferay archive not exists, downloading..."
+    wget $LR_DOWNLOAD_PATH -O $INSTALLS_FOLDER/$LR_FILE
+  fi
 
-    if [ -d $LR_PATH ]; then
-  	   ln -s $LR_PATH /home/$LR_USER/liferay
+	echo "Unzipping..."
+	unzip -q $INSTALLS_FOLDER/$LR_FILE -d /home/$LR_USER/
 
-       if [ -f /vagrant/resources/configs/portal-ext.properties ]; then
-         cp /vagrant/resources/configs/portal-ext.properties /home/$LR_USER/liferay
-       fi
+  LR_PATH=`find /home/$LR_USER/ -type d -iname 'liferay-portal*'`
+  echo "Liferay installed in: \"$LR_PATH\""
 
+  if [ -d $LR_PATH ]; then
+	   ln -s $LR_PATH /home/$LR_USER/liferay
+
+     if [ -f $CONFIGS_FOLDER/liferay/portal-ext.properties ]; then
        echo "Copying portal-ext.properties to \"$LR_PATH\""
-       cp /vagrant/resources/configs/liferay/portal-ext.properties $LR_PATH/
+       cp $CONFIGS_FOLDER/liferay/portal-ext.properties $LR_PATH/
+     fi
 
-       echo "Copying setenv.sh to \"$LR_PATH/tomcat-7.0.62/bin/\""
-       cp /vagrant/resources/configs/liferay/tomcat/bin/setenv.sh $LR_PATH/tomcat-7.0.62/bin/
+     echo "Copying setenv.sh to \"$LR_PATH/tomcat-7.0.62/bin/\""
+     cp $CONFIGS_FOLDER/liferay/tomcat/bin/setenv.sh $LR_PATH/tomcat-7.0.62/bin/
 
-       sudo chown -R $LR_USER:$LR_USER /home/$LR_USER/
-    fi
-
-	else
-		echo "Liferay archive not exists, skipping"
-	fi
-
+     sudo chown -R $LR_USER:$LR_USER /home/$LR_USER/
+  fi
 fi
 echo "Done."
--- Vagrant/scripts/install_oracle_java.sh
+++ Vagrant/scripts/install_oracle_java.sh
@@ -17,18 +17,20 @@ else
 	J_MINOR=$2
 	J_BUILD=$3
 
+  INSTALLS_FOLDER=/vagrant/resources/installs
+
 	JDK_RPM="jdk-"$J_MAJOR"u"$J_MINOR"-linux-x64.rpm"
 	JDK_URL="http://download.oracle.com/otn-pub/java/jdk/"$J_MAJOR"u"$J_MINOR"-b"$J_BUILD"/jdk-"$J_MAJOR"u"$J_MINOR"-linux-x64.rpm"
 
-	if [ -f /vagrant/resources/installs/$JDK_RPM ]
+	if [ -f $INSTALLS_FOLDER/$JDK_RPM ]
 		then
 		echo "Java JDK $JDK_RPM exists, installing..."
 	else
 		echo "Java JDK $JDK_RPM not exists, downloading..."
-		wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O /vagrant/$JDK_RPM $JDK_URL
+		wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -O $INSTALLS_FOLDER/$JDK_RPM $JDK_URL
 	fi
 
-	yum -y --nogpgcheck localinstall /vagrant/resources/installs/$JDK_RPM
+	yum -y --nogpgcheck localinstall $INSTALLS_FOLDER/$JDK_RPM
 
 fi
 echo "Done."
